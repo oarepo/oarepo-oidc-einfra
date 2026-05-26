9cd19b261ce09f0692be4a90ae85a622d2ed9df3
Mirek Simek <miroslav.simek@cesnet.cz>
2026-05-26 07:11:34 +0200

    Upgrading invenio - request validation changed

--- oarepo_oidc_einfra/services/requests/invitation.py
+++ oarepo_oidc_einfra/services/requests/invitation.py
@@ -9,14 +9,19 @@
 
 from __future__ import annotations
 
-from typing import ClassVar
+from typing import TYPE_CHECKING, ClassVar, override
 
 from invenio_communities.members.services.request import CommunityInvitation
 from invenio_i18n import lazy_gettext as _
 from marshmallow import fields
+from oarepo_requests.types import DefaultReceiverMixin
+
+if TYPE_CHECKING:
+    from flask_principal import Identity
+    from invenio_records.api import Record
 
 
-class AAICommunityInvitation(CommunityInvitation):
+class AAICommunityInvitation(DefaultReceiverMixin, CommunityInvitation):
     """AAI backed invitation request."""
 
     type_id = "aai-community-invitation"
@@ -29,4 +34,16 @@ class AAICommunityInvitation(CommunityInvitation):
     payload_schema: ClassVar[dict] = {  # type: ignore[reportIncompatibleVariableOverride]
         # Identifier of the invitation request from the AAI system
         "aai_id": fields.String(),
+        "user": fields.Integer(),
     }
+
+    @override
+    @classmethod
+    def default_request_receiver(
+        cls,
+        identity: Identity,
+        topic: Record,
+        creator: dict[str, str] | Identity,
+        data: dict,
+    ) -> dict[str, str] | None:
+        return None
