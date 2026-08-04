c25c58abff9cb1a00cd0b5da9c81cf9088dd3636
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-08-04 10:36:15 +0200

    fix: change log error to exception

--- oarepo_oidc_einfra/services/providers/token_exchange.py
+++ oarepo_oidc_einfra/services/providers/token_exchange.py
@@ -74,7 +74,7 @@ class EInfraTokenExchangeProvider(AuthProvider):
                 options={"require": ["sub", "exp", "iat"]},
             )
         except jwt.PyJWTError:
-            log.error("Rejected e-INFRA token: JWT validation failed")
+            log.exception("Rejected e-INFRA token: JWT validation failed")
             abort(403)
 
     @staticmethod
